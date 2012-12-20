3ec4a9473efbaff5b3bdd090f222c93bdc972b2c
Mathieu Lutfy <mathieu@bidon.ca>
2012-12-20 09:35:22 -0500

    Add support to enable PCP-Teams per contribution page.

--- pcpteams.php
+++ pcpteams.php
@@ -68,3 +68,101 @@ function pcpteams_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 function pcpteams_civicrm_managed(&$entities) {
   return _pcpteams_civix_civicrm_managed($entities);
 }
+
+/**
+ * Implements hook_civicrm_buildForm().
+ */
+function pcpteams_civicrm_buildForm($formName, &$form) {
+  $f = 'pcpteams_civicrm_buildForm_' . $formName;
+
+  if (function_exists($f)) {
+    $f($form);
+  }
+}
+
+/**
+ * Form: CRM_PCP_Form_Contribute
+ * Description: PCP configuration for a contribution page. Allow to enable PCP teams per form.
+ * See: pcpteams_civicrm_buildForm()
+ */
+function pcpteams_civicrm_buildForm_CRM_PCP_Form_Contribute(&$form) {
+  $form->addElement('checkbox', 'pcp_team_active', ts('Enable Personal Campaign Pages for Teams?'));
+
+  $target_entity_table = CRM_Utils_Array::value('target_entity_table', $form->_defaultValues);
+  $target_entity_id    = CRM_Utils_Array::value('target_entity_id', $form->_defaultValues);
+  $pcp_team_active     = pcpteams_pcpblockteam_getvalue($target_entity_table, $target_entity_id);
+
+  $defaults = array(
+    'pcp_team_active' => $pcp_team_active,
+  );
+
+  $form->setDefaults($defaults);
+
+  CRM_Core_Region::instance('pcp-fields')->add(array(
+    'template' => 'CRM/Pcpteams/ContributionPageSetup.tpl',
+  ));
+}
+
+/**
+ * Implements hook_civicrm_postProcess().
+ */
+function pcpteams_civicrm_postProcess($formName, &$form) {
+  switch($formName) {
+    case 'CRM_PCP_Form_Contribute':
+      $target_entity_type = CRM_Utils_Array::value('target_entity_type', $form->_defaultValues);
+      $target_entity_id   = CRM_Utils_Array::value('target_entity_id', $form->_defaultValues);
+      $pcp_team_active    = CRM_Utils_Array::value('pcp_team_active', $form->_submitValues);
+
+      pcpteams_pcpblockteam_setvalue($target_entity_type, $target_entity_id, $pcp_team_active);
+
+      break;
+  }
+}
+
+/**
+ * Returns the current pcpblock "team" is_active value.
+ * e.g. whether the form allows PCP by teams.
+ */
+function pcpteams_pcpblockteam_getvalue($target_entity_type, $target_entity_id) {
+  $pcpblock = new CRM_PCP_DAO_PCPBlock();
+  $pcpblock->target_entity_type = $target_entity_type;
+  $pcpblock->target_entity_id = $target_entity_id;
+
+  if ($pcpblock->find(TRUE)) {
+    $dao = CRM_Core_DAO::executeQuery("SELECT * FROM civicrm_pcp_block_team WHERE civicrm_pcp_block_id = " . $pcpblock->id);
+
+    if ($dao->fetch()) {
+      return $dao->is_active;
+    }
+  }
+
+  return FALSE;
+}
+
+/**
+ * Sets the current pcpblock "team" is_active value.
+ * e.g. whether the form allows PCP by teams.
+ */
+function pcpteams_pcpblockteam_setvalue($target_entity_type, $target_entity_id, $pcp_team_active) {
+  $pcpblock = new CRM_PCP_DAO_PCPBlock();
+  $pcpblock->target_entity_type = $target_entity_type;
+  $pcpblock->target_entity_id = $target_entity_id;
+
+  $pcp_team_active = intval($pcp_team_active);
+
+  if ($pcpblock->find(TRUE)) {
+    $dao = CRM_Core_DAO::executeQuery("SELECT * FROM civicrm_pcp_block_team WHERE civicrm_pcp_block_id = " . $pcpblock->id);
+
+    if ($dao->fetch()) {
+      CRM_Core_DAO::executeQuery("UPDATE civicrm_pcp_block_team SET is_active = " . $pcp_team_active . " WHERE civicrm_pcp_block_id = " . $dao->civicrm_pcp_block_id);
+    }
+    else {
+      CRM_Core_DAO::executeQuery("INSERT INTO civicrm_pcp_block_team (civicrm_pcp_block_id, is_active)
+                                  VALUES ({$pcpblock->id}, $pcp_team_active)");
+    }
+  }
+  else {
+    CRM_Core_Error::fatal(ts('Could not find the PCPBlock for entity: %1 %2', array(1 => $target_entity_id, 2 => $target_entity_type)));
+  }
+}
+
---
+++ templates/CRM/Pcpteams/ContributionPageSetup.tpl
@@ -0,0 +1,6 @@
+<table class="form-layout">
+  <tr  class="crm-contribution-contributionpage-pcp-form-block-pcp_team_active">
+      <td class="label">&nbsp;</td>
+      <td>{$form.pcp_team_active.html} {$form.pcp_team_active.label}</td>
+  </tr>
+</table>
