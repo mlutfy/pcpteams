bfa058d4e126bbbdbb0d0c4a4f170e44e4318ef2
Mathieu Lutfy <mathieu@bidon.ca>
2013-01-14 11:18:02 -0500

    Seems like civi has issues including the .sql file if it has comments glued to the first line of the 'create'..

--- sql/pcpteams-install.sql
+++ sql/pcpteams-install.sql
@@ -1,15 +1,17 @@
 --
--- Creates the table associating a `civicrm_pcp` page to a parent team page.
+-- Creates the table associating a civicrm_pcp page to a parent team page.
 -- Note: when a parent PCP page is deleted, the child becomes an individual without a team.
 --
+
 CREATE TABLE `civicrm_pcp_team` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Personal Campaign Team ID',
   `civicrm_pcp_id` int(10) unsigned NOT NULL COMMENT 'FK to Personal Campaign Page ID',
-  `civicrm_pcp_id_parent` int(10) unsigned NULL COMMENT 'FK to parent Personal Campaign Page ID',
+  `civicrm_pcp_id_parent` int(10) unsigned DEFAULT NULL COMMENT 'FK to parent Personal Campaign Page ID',
   `status_id` int(10) unsigned NOT NULL COMMENT 'Whether the team relation is active, pending, rejected.',
   `type_id` int(10) unsigned NOT NULL COMMENT 'Individual=1 or team=2 page.',
   PRIMARY KEY (`id`),
   KEY `FK_civicrm_pcp_id` (`civicrm_pcp_id`),
+  KEY `FK_civicrm_pcp_id_parent` (`civicrm_pcp_id_parent`),
   CONSTRAINT `FK_civicrm_pcp_id` FOREIGN KEY (`civicrm_pcp_id`) REFERENCES `civicrm_pcp` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_civicrm_pcp_id_parent` FOREIGN KEY (`civicrm_pcp_id_parent`) REFERENCES `civicrm_pcp` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -17,6 +19,7 @@ CREATE TABLE `civicrm_pcp_team` (
 --
 -- Settings for the PCP Block, e.g. whether teams are allowed or not
 --
+
 CREATE TABLE `civicrm_pcp_block_team` (
   `civicrm_pcp_block_id` int(10) unsigned NOT NULL COMMENT 'FK to PCP block Id',
   `is_active` tinyint(4) DEFAULT '1' COMMENT 'Is Personal Campaign Team functionality enabled/active?',
