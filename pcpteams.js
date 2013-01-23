352113a4d95f010f4b80b560ac3c89d701fb46be
Mathieu Lutfy <mathieu@bidon.ca>
2013-01-23 16:51:10 -0500

    Fix team title show/hide depending on if team/indiv.

--- pcpteams.js
+++ pcpteams.js
@@ -2,12 +2,15 @@
 // NB: the link the person received may include a pre-set team ID
 function pcpteams_profile_toggle_title() {
   if (cj('#pcp_team_id').val()) {
+    // Has a team, so hide the title, the title will be his/her name
     cj('.crm-contribution-form-block-title').hide();
-    cj('#pcp_title').val(cj('input[name="pcp_team_default_title"]').val());
+    if (cj('input[name="pcp_team_default_title"]').length > 0) {
+      cj('#pcp_title').val(cj('input[name="pcp_team_default_title"]').val());
+    }
   }
   else {
+    // Team page, or individual without a team
     cj('.crm-contribution-form-block-title').slideDown('slow');
-    cj('#pcp_title').val('');
   }
 }
 
