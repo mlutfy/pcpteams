e0704dc950da70335c3e4b520faa0a29916d177c
Mathieu Lu <mathieu@symbiotic.coop>
2024-08-28 17:48:33 -0400

    Version 1.5.0, regen civix, bump min-req compat 5.70

--- pcpteams.php
+++ pcpteams.php
@@ -13,15 +13,6 @@ function pcpteams_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function pcpteams_civicrm_xmlMenu(&$files) {
-  _pcpteams_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function pcpteams_civicrm_install() {
@@ -29,47 +20,10 @@ function pcpteams_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function pcpteams_civicrm_uninstall() {
-  return _pcpteams_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function pcpteams_civicrm_enable() {
   return _pcpteams_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function pcpteams_civicrm_disable() {
-  return _pcpteams_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function pcpteams_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _pcpteams_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- */
-function pcpteams_civicrm_managed(&$entities) {
-  return _pcpteams_civix_civicrm_managed($entities);
 }
 
 /**
@@ -220,13 +174,4 @@ function pcpteams_civicrm_pageRun(&$page) {
 
       break;
   }
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function pcpteams_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _pcpteams_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
